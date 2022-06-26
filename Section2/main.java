f944bc3c75d6336d9bd6e065f3fc271d59c8ae7b
SangSoo <tkdtn1427@gmail.com>
2022-06-26 12:47:10 +0900

    daily-Coding-last(ing)

--- Section2/main.java
+++ Section2/main.java
@@ -2,39 +2,46 @@ package Section2;
 import java.util.*;
 
 public class main {
+//    public static void main(String[] args) {
+//        boolean output2 = balancedBrackets("())()(()");
+//        System.out.println(output2);
+//    }
+//    static boolean balancedBrackets(String str) {
+//        ArrayList<Character> stack = new ArrayList<Character>();
+//        ArrayList<String> open_ = new ArrayList<>(Arrays.asList(new String[]{"[","{","("}));
+//        ArrayList<String> close_ = new ArrayList<>(Arrays.asList(new String[]{"]","}",")"}));
+//
+//        for(int i=0; i<str.length(); i++){
+//            if(isVaild(str.charAt(i),open_)){
+//                stack.add(str.charAt(i));
+//            }else if(isVaild(str.charAt(i),close_)){
+//                if(stack.size()==0 || !isMatch(stack.remove(stack.size()-1),str.charAt(i))){
+//                    return false;
+//                }
+//            }else{}
+//        }
+//        if(stack.isEmpty()) return true;
+//        else return false;
+//    }
+//
+//    static boolean isVaild(char str,ArrayList<String> arr){
+//        if(arr.contains(String.valueOf(str)))
+//            return true;
+//        else return false;
+//    }
+//
+//    static boolean isMatch(char stack, char str){
+//        if(stack=='(' && str==')') return true;
+//        else if(stack=='[' && str==']') return true;
+//        else if(stack=='{' && str=='}') return true;
+//        else return false;
+//    }
+
+//=============================================================
+
     public static void main(String[] args) {
-        boolean output2 = balancedBrackets("())()(()");
-        System.out.println(output2);
-    }
-    static boolean balancedBrackets(String str) {
-        ArrayList<Character> stack = new ArrayList<Character>();
-        ArrayList<String> open_ = new ArrayList<>(Arrays.asList(new String[]{"[","{","("}));
-        ArrayList<String> close_ = new ArrayList<>(Arrays.asList(new String[]{"]","}",")"}));
+        boolean[] test = new boolean[20];
 
-        for(int i=0; i<str.length(); i++){
-            if(isVaild(str.charAt(i),open_)){
-                stack.add(str.charAt(i));
-            }else if(isVaild(str.charAt(i),close_)){
-                if(stack.size()==0 || !isMatch(stack.remove(stack.size()-1),str.charAt(i))){
-                    return false;
-                }
-            }else{}
-        }
-        if(stack.isEmpty()) return true;
-        else return false;
-    }
-
-    static boolean isVaild(char str,ArrayList<String> arr){
-        if(arr.contains(String.valueOf(str)))
-            return true;
-        else return false;
-    }
-
-    static boolean isMatch(char stack, char str){
-        if(stack=='(' && str==')') return true;
-        else if(stack=='[' && str==']') return true;
-        else if(stack=='{' && str=='}') return true;
-        else return false;
     }
 }
 

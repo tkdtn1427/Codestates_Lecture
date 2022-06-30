916b8741d2a5a43e5d203b4407bd5521c084f6c3
SangSoo <tkdtn1427@gmail.com>
2022-06-30 18:13:54 +0900

    DailyCoding

--- Section2/DailyCoding/_25_power.java
+++ Section2/DailyCoding/_25_power.java
@@ -8,16 +8,33 @@ public class _25_power {
         System.out.println(output);
     }
 
-    static long power(int base, int exponent) {
-        long result = 1;
-        for(int i=0; i<exponent; i++){
-            result *= base;
-            if(result >= 94906249){
-                result %= 94906249;
-            }
-        }
+//    static long power(int base, int exponent) {
+//        long result = 1;
+//        for(int i=0; i<exponent; i++){
+//            result *= base;
+//            if(result >= 94906249){
+//                result %= 94906249;
+//            }
+//        }
+//
+//        return result;
+//    }
 
-        return result;
+//==================================================
+
+    static long power(int base, int exponent) {
+        //재귀함수로 동작합니다.
+        //exponent변수가 0이 될때까지 재귀를 돈 이후, 나머지 연산을 계산하고,
+        //해당 결과값을 사용하여 홀수일 경우 다시 연산하여 리턴합니다.
+
+        if(exponent == 0) return 1;
+
+        int half = exponent / 2;
+        long temp = power(base, half);
+        long result = (temp * temp) % 94906249;
+
+        if(exponent % 2 == 1) return (base * result) % 94906249;
+        else return result;
     }
 
 }

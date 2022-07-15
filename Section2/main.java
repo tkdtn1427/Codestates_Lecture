aae6ddfcb756203c1a97c6c6dfa5da4c7f4906d7
SangSoo <tkdtn1427@gmail.com>
2022-07-15 14:12:30 +0900

    dailyCoding

--- Section2/main.java
+++ Section2/main.java
@@ -1,11 +1,36 @@
 package Section2;
+import java.lang.reflect.Member;
 import java.util.*;
 
 public class main {
-//    public static void main(String[] args) {
-//        boolean output2 = balancedBrackets("())()(()");
-//        System.out.println(output2);
-//    }
+    public static void main(String[] args) {
+//        Map<String,Integer> trans = new HashMap<String,Integer>(){{
+//            put("A",0);
+//            put("G",1);
+//            put("C",2);
+//            put("T",3);
+//        };
+//        };
+//
+//        String[][] test = {{"A","C","A","G"},
+//                {"C","G","T","A"},
+//                {"A","T","C","G"},
+//                {"G","A","G","T"}};
+//
+//        String test_str = "AAGTCG";
+//        StringBuffer temp = new StringBuffer(test_str);
+//        test_str = temp.reverse().toString();
+//
+//        String first = String.valueOf(test_str.charAt(0));
+//
+//        for(int i=1; i<test_str.length(); i++){
+//            String next = String.valueOf(test_str.charAt(i));
+//            first = test[trans.get(first)][trans.get(next)];
+//        }
+//
+//        System.out.println(first);
+
+    }
 //    static boolean balancedBrackets(String str) {
 //        ArrayList<Character> stack = new ArrayList<Character>();
 //        ArrayList<String> open_ = new ArrayList<>(Arrays.asList(new String[]{"[","{","("}));
@@ -39,9 +64,18 @@ public class main {
 
 //=============================================================
 
-    public static void main(String[] args) {
-        boolean[] test = new boolean[20];
-
-    }
+//    public static void main(String[] args) {
+//        boolean[] tile = new boolean[4];
+//        Arrays.fill(tile, Boolean.TRUE);
+//        int count = 1;
+//
+//        for (int i = 2; i <= tile.length - 1; i++) {
+//            count += tofill(Arrays.copyOfRange(tile,0,i));
+//        }
+//    }
+//
+//    static int tofill(boolean[] tile){
+//
+//
 }
 

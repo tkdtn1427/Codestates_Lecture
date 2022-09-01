1104310acbc73511f36eaad64dd4d701682399da
[Sangsoo Lee] <tkdtn1427@gmail.com>
2022-09-01 18:08:06 +0900

    dailyCoding

---
+++ Section2/DailyCoding2/test.java
@@ -0,0 +1,10 @@
+package Section2.DailyCoding2;
+
+import java.util.Arrays;
+
+public class test {
+    public static void main(String[] args) {
+        String str = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJjb3Mgand0IHRva2VuIiwibmlja05hbWUiOiLquYDsvZTrlKkiLCJleHAiOjE2NjE4NjYxOTMsImVtYWlsIjoiY29kZXN0YXRkZXNAbmF2ZXIuY29tIn0.2uIVTUwLo-ul2Xx53piefnBfX0bSSXEq2skoRpDx5CjLxmgVOWxZ9tlX8EJ9xOlV0T9if-v7nWAz_Y2Fx6fhYA";
+
+    }
+}

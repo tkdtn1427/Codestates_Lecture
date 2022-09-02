c0fc090fddfe8b8b2f421a7745bf1bcfb497ae0f
[Sangsoo Lee] <tkdtn1427@gmail.com>
2022-09-02 18:49:20 +0900

    daily

--- Section2/DailyCoding2/test.java
+++ Section2/DailyCoding2/test.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 
 public class test {
     public static void main(String[] args) {
-        String str = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJjb3Mgand0IHRva2VuIiwibmlja05hbWUiOiLquYDsvZTrlKkiLCJleHAiOjE2NjE4NjYxOTMsImVtYWlsIjoiY29kZXN0YXRkZXNAbmF2ZXIuY29tIn0.2uIVTUwLo-ul2Xx53piefnBfX0bSSXEq2skoRpDx5CjLxmgVOWxZ9tlX8EJ9xOlV0T9if-v7nWAz_Y2Fx6fhYA";
-
+        StringBuilder br = new StringBuilder();
     }
 }

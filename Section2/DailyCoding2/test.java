9eec172fbb164b980c5e01086067579c62e695ca
[Sangsoo Lee] <tkdtn1427@gmail.com>
2022-09-04 23:36:28 +0900

    dailyCoding

--- Section2/DailyCoding2/test.java
+++ Section2/DailyCoding2/test.java
@@ -1,12 +1,15 @@
 package Section2.DailyCoding2;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
 
 public class test {
     public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
 
     }

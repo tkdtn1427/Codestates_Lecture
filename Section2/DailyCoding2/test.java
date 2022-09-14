ed51c62d2d8071a528d4754b4626cb6d12e159c8
[Sangsoo Lee] <tkdtn1427@gmail.com>
2022-09-14 23:15:07 +0900

    dailyCoding

--- Section2/DailyCoding2/test.java
+++ Section2/DailyCoding2/test.java
@@ -5,15 +5,23 @@ import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
+import java.util.Scanner;
 
 //Solution
 public class test {
     public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder(br.readLine());
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        String str2 = scanner.nextLine();
+        String[] strlst = new String[]{str, str2};
+        int idx = 0;
 
-        bw.flush();
-        bw.close();
+        for(String tmp : strlst){
+            if(tmp.length() ==0) strlst[idx] = "null";
+            idx++;
+        }
+
+
+        System.out.println(Arrays.toString(strlst));
     }
 }

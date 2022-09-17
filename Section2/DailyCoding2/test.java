f9e24edbde3d1ad37d2611eb346517364feb8d22
[Sangsoo Lee] <tkdtn1427@gmail.com>
2022-09-18 00:15:52 +0900

    fix : ing

--- Section2/DailyCoding2/test.java
+++ Section2/DailyCoding2/test.java
@@ -10,18 +10,16 @@ import java.util.Scanner;
 //Solution
 public class test {
     public static void main(String[] args) throws Exception{
-        Scanner scanner = new Scanner(System.in);
-        String str = scanner.nextLine();
-        String str2 = scanner.nextLine();
-        String[] strlst = new String[]{str, str2};
-        int idx = 0;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        for(String tmp : strlst){
-            if(tmp.length() ==0) strlst[idx] = "null";
-            idx++;
-        }
+//        String[] strlst = new String[]{str, str2};
+//        int idx = 0;
+//
+//        for(String tmp : strlst){
+//            if(tmp.length() ==0) strlst[idx] = "null";
+//            idx++;
+//        }
 
 
-        System.out.println(Arrays.toString(strlst));
     }
 }

bae9bbce7382669479f4484611b4f36f93d5ec40
[Sangsoo Lee] <tkdtn1427@gmail.com>
2022-09-21 21:24:57 +0900

    daily

--- Section2/DailyCoding2/test.java
+++ Section2/DailyCoding2/test.java
@@ -4,7 +4,10 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.Scanner;
 
 //Solution
@@ -12,14 +15,9 @@ public class test {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-//        String[] strlst = new String[]{str, str2};
-//        int idx = 0;
-//
-//        for(String tmp : strlst){
-//            if(tmp.length() ==0) strlst[idx] = "null";
-//            idx++;
-//        }
-
-
+        Date test = new Date();
+        System.out.println(test);
+        int test2 = test.getDay();
+        System.out.println(test2);
     }
 }
